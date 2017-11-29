e4585e0249de1a4827f04ff0e86822324644ecfc
Anay Khator <Anaykhator@users.noreply.github.com>
2017-11-29 16:27:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anay/stockmaintenance/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.anay.stockmaintenance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void act(View view)
+    {
+        Intent intent = new Intent(this,NewBill.class);
+        this.startActivity(intent);
+    }
+}
